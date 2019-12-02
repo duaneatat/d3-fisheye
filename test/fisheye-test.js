2dd0595bc3b63fc28d81a3862ebb9c7852cdbda3
duaneatat <dubarlow@tetrationanalytics.com>
2019-12-02 00:52:02 -0800

    Implement radial version with smoothing defaulted

---
+++ test/fisheye-test.js
@@ -0,0 +1,10 @@
+var tape = require('tape'),
+  fisheye = require('../');
+
+tape(
+  'fisheye() returns the answer to the ultimate question of life, the universe, and everything.',
+  function(test) {
+    test.equal(42, 42);
+    test.end();
+  }
+);
